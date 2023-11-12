1d4688606a1a46c0a74890ac517f4776e41e4960
Sowmya <chattisowmya@gmail.com>
2023-11-12 13:41:54 -0500

    checking only if my dag is present

--- test/test_dags.py
+++ test/test_dags.py
@@ -5,7 +5,7 @@ def test_no_import_errors():
   dag_list = []
   dag_bag = DagBag(dag_folder='dags/', include_examples=False)
   
-  for dag_id in dag_bag:
+  for dag_id in dag_bag.dags:
         dag_list.append(dag_id)
         
   assert len(dag_list) == 1, "only custom dag must be present"      
