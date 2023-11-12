ba6afafa2bda1cf350edf73d6377a982c65824b6
Sowmya <chattisowmya@gmail.com>
2023-11-12 12:19:11 -0500

    moved preprocess file into dags folder

--- test/test_dags.py
+++ test/test_dags.py
@@ -4,3 +4,5 @@ from airflow.models import DagBag
 def test_no_import_errors():
   dag_bag = DagBag(dag_folder='dags/', include_examples=False)
   assert len(dag_bag.import_errors) == 0, "No Import Failures"
+  for dag_id, exception in dag_bag.import_errors.items():
+        print(f"Import error for DAG ID '{dag_id}': {str(exception)}")
