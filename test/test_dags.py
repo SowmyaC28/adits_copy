7fb89393060d30aac862fe6755263e9c99e164ed
Sowmya <chattisowmya@gmail.com>
2023-11-12 13:48:52 -0500

    checking dag list and task list count

--- test/test_dags.py
+++ test/test_dags.py
@@ -1,11 +1,22 @@
 import pytest
 from airflow.models import DagBag
 
-def test_no_import_errors():
+def check_dag_list():
   dag_list = []
   dag_bag = DagBag(dag_folder='dags/', include_examples=False)
   
   for dag_id in dag_bag.dags:
         dag_list.append(dag_id)
         
-  assert len(dag_list) == 1, "only custom dag must be present"      
+  assert len(dag_list) == 1, "only custom dag must be present"  
+  
+def check_task_list():
+  dag_list = []
+  dag_bag = DagBag(dag_folder='dags/', include_examples=False)
+  task_list = []
+  for dag_id in dag_bag.dags:
+    dag = dag_bag.get_dag(dag_id)
+    for tasks in dag.tasks:
+         task_list.append(tasks)
+        
+  assert len(task_list) == 2, "2 dummy tasks must be present"       
